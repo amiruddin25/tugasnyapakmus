d8b44c9314c16452d2edf875a7ebe05ba90258dc
amiruddin25 <57261528+amiruddin25@users.noreply.github.com>
2019-11-01 23:15:49 +0800

    java1

---
+++ MUH.AMIRUDDIN/src/menghitungluaskubus.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sony vaio;
+ */import java.util.Scanner;
+public class menghitungluaskubus {
+    public static void main(String[] args) {
+        double sisi,luaspermukaan,volume,luassisikubus;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan Panjang Sisi: ");
+        sisi=input.nextDouble();
+        luassisikubus=sisi*sisi;
+        System.out.println("luas sisikubusnya adalah "+luassisikubus);
+        luaspermukaan=6*luassisikubus;
+        System.out.println("luas permukaannya adalah: "+luaspermukaan);
+        
+        System.out.println("Mencari volume!!!");
+        volume=sisi*sisi*sisi;
+        System.out.print("volumenyass adlah: "+ volumes);
+        
+        
+        
+        
+        
+        
+    }
+}
